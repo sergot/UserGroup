0c39a7fd7070441de1267287085058e37f29d8c2
sergot <filip@sergot.pl>
2013-11-28 22:07:02 +0100

    config added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UserGroup::Application.config.secret_key_base = 'd91afe6316db578e40f875af2550ca5ec5114b37e5918ba768b706020414b91080e0679485a8168582d2802a0e269259430e0003b4588d7505e4412283e018ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UserGroup::Application.config.session_store :cookie_store, key: '_UserGroup_session'
